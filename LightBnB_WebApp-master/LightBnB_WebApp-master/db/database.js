c9805bc4ce975a023699760f8070805e69721189
Hamanpreet <haman42626@gmail.com>
2023-07-09 11:56:30 -0400

    Add special rates and guest_reviews table(stretch)

--- LightBnB_WebApp-master/LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/LightBnB_WebApp-master/db/database.js
@@ -76,7 +76,7 @@ const getAllProperties = function (options, limit = 10) {
   LIMIT $1;
   `,[limit])
   .then(res => {
-    console.log(res.rows);
+   
     return res.rows;
   })
   .catch(err => console.error(err.message));
